91b01cd42b6508b349d0341844e10eb5913c4c41
Your Name <you@example.com>
2022-10-16 00:43:13 +0200

    Added condition if domain is found print location, host, and mail addresses if Occitanie province is matched

--- email_finder.py
+++ email_finder.py
@@ -3,6 +3,9 @@
 # CENSYS
 # API key : 5669609e-8c8e-4a5e-a75a-5c7ba697169a
 # Secret : 3IdKh4MGJ4EQ93kzIrqYzr2nuyP53hvW
+
+# Search email adresses through Censys by request : location.province:Occitanie and location.city:Montpellier and services.port:80 or services.port:443
+
 from modules import (get_company_detail)
 from xml import dom
 from googleapiclient.discovery import build
@@ -28,5 +31,11 @@ for completedomain in results:
     domain='.'.join(domain[-2:])
     pprint.pprint("Website Domain : " + completedomain['displayLink'])
     pprint.pprint("Domain : " + domain)
-    get_company_detail(domain, True)
+    query = h.search(str(domain) + " and location.province:Occitanie", per_page=1)
+    domain_lookup_result = query()
+    if domain_lookup_result:
+        pprint.pprint("Location Domain : " + domain_lookup_result[0]['location']['city'] + " " + domain_lookup_result[0]['location']['postal_code'] + " " + domain_lookup_result[0]['location']['province'])
+        pprint.pprint("Machine founded : " + str(''.join(domain_lookup_result[0]['dns']['reverse_dns']['names'])))
+        if domain_lookup_result[0]['location']['province'] == "Occitanie":
+            get_company_detail(domain, True)
     print('\n')
