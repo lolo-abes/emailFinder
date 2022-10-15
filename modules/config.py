6f1e45d1451d7ce8bd47be3e520c8a3867c79d18
Your Name <you@example.com>
2022-10-15 21:20:08 +0200

    Add email hunter API key in config.py

--- modules/config.py
+++ modules/config.py
@@ -3,7 +3,7 @@
 ##############################
 
 clearbit_api_key = ""
-shodan_api_key = ""
+shodan_api_key = "qqtwpQZ4hxT6Z8KAYb1oGskoauLxx4nc"
 fullcontact_api_key = ""
 virus_total_api_key = ""
-email_hunter_api_key = ""
+email_hunter_api_key = "e49e01212dcb9fce7741a2942d7b5c7f2e0ed0fc"
