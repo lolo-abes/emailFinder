5595cbb9d6b3a76b77b0fce319e1104a2941e7d8
Your Name <you@example.com>
2022-10-15 21:19:30 +0200

    Add company detail feature in email_finder.py

--- email_finder.py
+++ email_finder.py
@@ -1,6 +1,9 @@
 # Google Cloud API KEY : AIzaSyC1fVY-mUK8hM4mn-vgb8ZkUhWjmULbfrM
 # CSE ID : 65672d610066e4c8f
-
+# CENSYS
+# API key : 5669609e-8c8e-4a5e-a75a-5c7ba697169a
+# Secret : 3IdKh4MGJ4EQ93kzIrqYzr2nuyP53hvW
+from modules import (get_company_detail)
 from xml import dom
 from googleapiclient.discovery import build
 import pprint
@@ -15,11 +18,12 @@ def google_search(search_term, api_key, cse_id, **kwargs):
     return res['items']
 
 results = google_search(
-    'mail site:*.fr', my_api_key, my_cse_id, num=10)
+    'abes site:*.fr', my_api_key, my_cse_id, num=10)
 
 for completedomain in results:
     domain=list(completedomain['displayLink'].split('.'))
     domain='.'.join(domain[-2:])
     pprint.pprint("Website Domain : " + completedomain['displayLink'])
     pprint.pprint("Domain : " + domain)
+    get_company_detail(domain, True)
     print('\n')
