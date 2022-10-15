9c159751c7bd8590bf40af468ea7c21d7580c662
Your Name <you@example.com>
2022-10-16 00:42:56 +0200

    Add Censys search engine API

--- email_finder.py
+++ email_finder.py
@@ -8,9 +8,12 @@ from xml import dom
 from googleapiclient.discovery import build
 import pprint
 import json
+from censys.search import CensysHosts
 
 my_api_key = "AIzaSyC1fVY-mUK8hM4mn-vgb8ZkUhWjmULbfrM"
 my_cse_id = "65672d610066e4c8f"
+
+h = CensysHosts()
 
 def google_search(search_term, api_key, cse_id, **kwargs):
     service = build("customsearch", "v1", developerKey=api_key)
